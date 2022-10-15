994701cd2bca7e8dc81d3fd30ee401c45ee24ab5
Maureen-Muriithi <moh2wanja@gmail.com>
2022-10-15 21:57:44 +0300

    Revert "Add teams page"
    This reverts commit 262fb7d3893cf50706e200ec1c24146d8ce6d9af.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { TaxmanagerComponent } from './taxmanager/taxmanager.component';
-import { TeamsComponent } from './teams/teams.component';
-
-const routes: Routes = [
-
-  {
-    path: 'team',
-    component: TeamsComponent
-  }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    TaxmanagerComponent,
-    TeamsComponent
+    TaxmanagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes),
-  ],
-  exports: [
-    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
