58af7c264069735f58a530108ea34f700a53d7d4
Maureen-Muriithi <moh2wanja@gmail.com>
2022-10-16 00:04:14 +0300

    Add teams page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TeamComponent } from './team/team.component';
+import { TaxmanagerComponent } from './taxmanager/taxmanager.component';
+
+const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: TaxmanagerComponent },
+  { path: 'team', component: TeamComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { TaxmanagerComponent } from './taxmanager/taxmanager.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    TaxmanagerComponent
+    TaxmanagerComponent,
+    TeamComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: TaxmanagerComponent},
+      {path: 'team', component: TeamComponent}
+    ]),  
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
